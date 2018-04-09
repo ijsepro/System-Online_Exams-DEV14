4ea66211e1a014b71c2d9614a6e417515f1e38ff
iroshanivimansa <irowickramarathna0818@gmail.com>
2018-04-09 10:32:13 -0700

    ES02

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ExamComponent } from './components/exam/exam.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExamComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
