4d47583631ed5d91fd1f0af0557fa727382d2b14
sachiniasanka <sachiniasanka020@gmail.com>
2018-04-23 13:08:11 +0530

    u

---
+++ FrontEnd/src/app/components/departmentregistration/departmentregistration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-departmentregistration',
+  templateUrl: './departmentregistration.component.html',
+  styleUrls: ['./departmentregistration.component.css']
+})
+export class DepartmentregistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
